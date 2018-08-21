edcb64ed5ff06108ae3599a3f12e2985c80537df
Jon Bonazza <jonbonazza@gmail.com>
2018-08-20 22:30:28 -0700

    Register SQLite custom type

--- addons/godot-tools.gdrpg/plugin.gd
+++ addons/godot-tools.gdrpg/plugin.gd
@@ -10,28 +10,24 @@ var tree_path = "res://test.dt"
 
 func _enter_tree():
 	# Custom types
+	add_custom_type("SQLite", "Node", preload("res://addons/godot-tools.gdrpg/lib/sqlite.gd"), null)
 	add_custom_type("DialogTree", "Node", preload("res://addons/godot-tools.gdrpg/dialog-system/dialog_tree_node.gd"), null)
 	
 	# Dialog Editor
 	rpgPlugin = preload("res://addons/godot-tools.gdrpg/RPGPlugin.tscn").instance()
 	rpgPlugin.hide()
 	get_editor_interface().get_editor_viewport().add_child(rpgPlugin)
-	#add_control_to_container(CONTAINER_TOOLBAR, rpgPlugin)
-	#get_editor_interface().get_editor_viewport().add_child(rpgPlugin)
-	#add_control_to_bottom_panel(dock, "Dialog Editor")
 	_create_gizmo()
 	
 func _exit_tree():
 	# Dialog Editor
 	remove_control_from_container(CONTAINER_TOOLBAR, gizmo)
 	get_editor_interface().get_editor_viewport().remove_child(rpgPlugin)
-	#get_editor_interface().get_editor_viewport().remove_child(rpgPlugin)
-	#remove_control_from_container(CONTAINER_TOOLBAR, rpgPlugin)
-	#remove_control_from_bottom_panel(rpgPlugin)
 	rpgPlugin.free()
 	
 	# Custom Types
 	remove_custom_type("DialogTree")
+	remove_custom_type("SQLite")
 
 func _create_gizmo():
 	gizmo = Button.new()
